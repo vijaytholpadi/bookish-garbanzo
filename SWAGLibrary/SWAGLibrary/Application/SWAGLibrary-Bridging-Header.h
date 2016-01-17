528dc5eca1903d379ea1485f154451ad13a5d37c
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-17 10:41:54 +0530

    Adding bridging header for inter-operability.

---
+++ SWAGLibrary/SWAGLibrary/Application/SWAGLibrary-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SWAGLibrary-Bridging-Header.h
+//  SWAGLibrary
+//
+//  Created by Vijay Tholpadi on 1/17/16.
+//  Copyright © 2016 TheGeekProjekt. All rights reserved.
+//
+
+#ifndef SWAGLibrary_Bridging_Header_h
+#define SWAGLibrary_Bridging_Header_h
+
+//#import "AFNetworking.h"
+
+
+#endif /* SWAGLibrary_Bridging_Header_h */
