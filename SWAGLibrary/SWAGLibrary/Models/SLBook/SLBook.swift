1bb9a58d00e27f98ea8aa4c1be91b1a79156f841
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-17 18:23:42 +0530

    Refactoring SLBook Model to make it more robust

--- SWAGLibrary/SWAGLibrary/Models/SLBook/SLBook.swift
+++ SWAGLibrary/SWAGLibrary/Models/SLBook/SLBook.swift
@@ -10,36 +10,40 @@ import Foundation
 
 class SLBook: NSObject {
     
-    var title : String?
-    var author : String?
+    var title : String
+    var author : String
     var category : String?
     var publisher : String?
     var lastCheckedOutDate : String?
     var lastCheckedOutBy : String?
     var urlString : String?
     
-    override init() {
-        super.init()
-    }
-    
-    init(dictionary: NSDictionary) {
-        
+    init(title: String, author: String) {
+        self.title = title
+        self.author = author
     }
     
     class func getBookArrayFromRawArray(rawArray:[[String:AnyObject]]) -> [SLBook] {
         
         var booksArray : [SLBook] = Array()
-        
             for item in rawArray {
-                let book : SLBook = SLBook.init()
-                book.title = item["title"] as? String
-                book.author = item["author"] as? String
-                book.category = item["categories"] as? String
+                let book : SLBook = SLBook.init(title: (item["title"] as? String)!, author: (item["author"] as? String)!)
+            
                 book.publisher = item["publisher"] as? String
-                book.lastCheckedOutDate = item["lastCheckedOut"] as? String
-                book.lastCheckedOutBy = item["lastCheckedOutBy"] as? String
-                book.urlString = item["url"] as? String
                 
+                if let category = item["categories"]{
+                    book.category = category as? String
+                }
+                
+                if let lastCheckedOutDate = item["lastCheckedOut"]{
+                    book.lastCheckedOutDate = lastCheckedOutDate as? String
+                }
+                
+                if let lastCheckedOutBy = item["lastCheckedOutBy"]{
+                    book.lastCheckedOutBy = lastCheckedOutBy as? String
+                }
+
+                book.urlString = item["url"] as? String
                 booksArray.append(book)
             }
         return booksArray
