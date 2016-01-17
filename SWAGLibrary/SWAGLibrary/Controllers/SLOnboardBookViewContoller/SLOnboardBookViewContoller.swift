ee640335b541418e647965dff8fb1c193739eb54
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-17 20:18:10 +0530

    Adding following functionality to SLOnboardBookViewController: Checking for mandatory fields and Adding the book to the server Incase of insufficient details, show an error to the user Dismiss view controller post successful addition

--- SWAGLibrary/SWAGLibrary/Controllers/SLOnboardBookViewContoller/SLOnboardBookViewContoller.swift
+++ SWAGLibrary/SWAGLibrary/Controllers/SLOnboardBookViewContoller/SLOnboardBookViewContoller.swift
@@ -18,6 +18,43 @@ class SLOnboardBookViewContoller: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }}    @IBAction func submitButtonPressed(sender: AnyObject) {    }    }
+    }
     
-        @IBAction func doneButtonPressed(sender: AnyObject) {    }
+    @IBAction func doneButtonPressed(sender: AnyObject) {
+        self.dismissViewController()
+    }
+    
+    @IBAction func submitButtonPressed(sender: AnyObject) {
+        if self.areMandatoryFieldsEmpty() {
+            self.showInsufficientDetailsAlert()
+        } else {
+            self.addBook()
+        }
+    }
+    
+    func addBook() {
+        VTNetworkingHelper.sharedInstance().performRequestWithPath(SLNetworkRoutes.postAddABook(), withAuth: true, forMethod: "POST", withRequestJSONSerialized: true, withParams: SLNetworkParams.postAddABookParamsWithAuthor(self.authorTextField.text!, category: self.categoriesTextField.text!, title: self.titleTextField.text!, publisher: self.publisherTextField.text!)) { (response:VTNetworkResponse!) -> Void in
+            if response.isSuccessful {
+                self.dismissViewController()
+            } else {
+                
+            }
+        }
+    }
+    
+    func areMandatoryFieldsEmpty() -> Bool {
+        return ((self.titleTextField.text?.isEmpty)! || (self.authorTextField.text?.isEmpty)!)
+    }
+    
+    func showInsufficientDetailsAlert() {
+        let insufficientDetailsAlertController = UIAlertController(title: "Error", message: "Title and Author fields are mandatory ", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        insufficientDetailsAlertController.addAction(okAction)
+        
+        presentViewController(insufficientDetailsAlertController, animated: true, completion: nil)
+    }
+    
+    func dismissViewController() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
