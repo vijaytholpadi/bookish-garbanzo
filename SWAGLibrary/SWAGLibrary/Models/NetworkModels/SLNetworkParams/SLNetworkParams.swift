5fea19401f4f12fa9040dc96ca898ad2f86559b9
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-17 14:55:07 +0530

    Adding SLNetworkParams file to provide parameters for API calls.

---
+++ SWAGLibrary/SWAGLibrary/Models/NetworkModels/SLNetworkParams/SLNetworkParams.swift
@@ -0,0 +1,19 @@
+//
+//  SLNetworkParams.swift
+//  SWAGLibrary
+//
+//  Created by Vijay Tholpadi on 1/17/16.
+//  Copyright © 2016 TheGeekProjekt. All rights reserved.
+//
+
+import Foundation
+
+class SLNetworkParams: NSObject {
+    class func postAddABookParamsWithAuthor(author : String, category : String, title : String, publisher : String) -> [String: String] {
+        return ["author":author, "categories":category, "title":title, "publisher":publisher]
+    }
+    
+    class func putUpdateBookParamsWithLastCheckedOutName(name : String) -> [String: String] {
+        return ["lastCheckedOutBy":name]
+    }
+}
