9abb9d26465872ed07938528ea6d436b01f63788
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-17 21:27:07 +0530

    Removing SDWebImage from Pods as it was not needed.

--- SWAGLibrary/Pods/Target Support Files/Pods-SWAGLibrary/Pods-SWAGLibrary-frameworks.sh
+++ SWAGLibrary/Pods/Target Support Files/Pods-SWAGLibrary/Pods-SWAGLibrary-frameworks.sh
@@ -53,9 +53,7 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods-SWAGLibrary/AFNetworking.framework'
-  install_framework 'Pods-SWAGLibrary/SDWebImage.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods-SWAGLibrary/AFNetworking.framework'
-  install_framework 'Pods-SWAGLibrary/SDWebImage.framework'
 fi
