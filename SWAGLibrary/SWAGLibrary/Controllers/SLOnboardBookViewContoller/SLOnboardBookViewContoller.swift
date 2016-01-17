77af6d90a66393d42fcc4b0826a19bc0e0127dec
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-17 20:15:33 +0530

    Adding SLOnboardBookViewController with Storyboard UI and base Swift file

---
+++ SWAGLibrary/SWAGLibrary/Controllers/SLOnboardBookViewContoller/SLOnboardBookViewContoller.swift
@@ -0,0 +1,23 @@
+//
+//  SLOnboardBookViewContoller.swift
+//  SWAGLibrary
+//
+//  Created by Vijay Tholpadi on 1/17/16.
+//  Copyright © 2016 TheGeekProjekt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SLOnboardBookViewContoller: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var publisherTextField: UITextField!
+    @IBOutlet weak var categoriesTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }}    @IBAction func submitButtonPressed(sender: AnyObject) {    }    }
+    
+        @IBAction func doneButtonPressed(sender: AnyObject) {    }
