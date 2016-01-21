b4cb453857e275da950de69e248664e55ea0c4da
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-21 21:17:49 +0530

    Commenting SLNetworkParams

--- SWAGLibrary/SWAGLibrary/Models/NetworkModels/SLNetworkParams/SLNetworkParams.swift
+++ SWAGLibrary/SWAGLibrary/Models/NetworkModels/SLNetworkParams/SLNetworkParams.swift
@@ -9,10 +9,12 @@
 import Foundation
 
 class SLNetworkParams: NSObject {
+    ///Class method returning parameters for Add a book API
     class func postAddABookParamsWithAuthor(author : String, category : String, title : String, publisher : String) -> [String: String] {
         return ["author":author, "categories":category, "title":title, "publisher":publisher]
     }
     
+    ///Class method returning parameters for update book API
     class func putUpdateBookParamsWithLastCheckedOutName(name : String) -> [String: String] {
         return ["lastCheckedOutBy":name]
     }
